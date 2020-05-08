270313939f700c67134054e31f369aaa91a7588a
chuliang014 <chuliang014@gmail.com>
2020-05-08 14:16:41 -0700

    update annotation

--- crawler.py
+++ crawler.py
@@ -6,7 +6,6 @@ import re
 import sys
 import threading
 import urllib.request as urllib
-from time import sleep
 
 import tweepy
 from urllib3.exceptions import ProtocolError, ReadTimeoutError
@@ -38,6 +37,7 @@ NUM_TWEETS = 10
 class MyStreamListener(tweepy.StreamListener):
 
     def __init__(self, q=queue.Queue()):
+        # initialize 4 tread for processing data of queue
         super(MyStreamListener, self).__init__()
         num_worker_threads = 4
         self.q = q
@@ -48,9 +48,12 @@ class MyStreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
         data = status._json
+        # just collect data in this function and no other processing
+        # because it may cause program stall or exception like incompleteRead
         self.q.put(data)
         return True
 
+    # this is for processing the data
     def do_stuff(self):
         while True:
             tweets_data = self.q.get()
@@ -64,14 +67,6 @@ class MyStreamListener(tweepy.StreamListener):
             return False
 
 
-myStreamListener = MyStreamListener()
-myStream = None
-
-mySecondStream = None
-myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
-mySecondStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
-
-
 def process(tweets_data):
     global fileName, tweetDB, currFile
     try:
@@ -80,6 +75,8 @@ def process(tweets_data):
         text = temp_text.encode('ascii', 'ignore')
         decoded_text = text.decode('utf8')
         tweets_data['title'] = None
+        # judge if the title include http
+        # if True, parsing the url to get the title.
         if "http" in tweets_data['text']:
             text = decoded_text
             url = re.search("(?P<url>https?://[^\s'\"]+)", text).group("url")
@@ -115,10 +112,12 @@ def process(tweets_data):
 
         tweetDB.write(json.dumps(tweets_data) + '\n')
 
+        # if reach 2G, teminate the program
         if currFile > maxNumFiles:
             myStream.disconnect()
-            mySecondStream.disconnect()
 
+        # check if current file is over 10MB
+        # if True, close file and create a new file for collecting
         statinfo = os.stat(fileName)
         if statinfo.st_size > max_size:
             tweetDB.close()
@@ -138,6 +137,10 @@ def process(tweets_data):
         print('vincent error {}'.format(e))
         pass
 
+#initialize stream
+myStreamListener = MyStreamListener()
+myStream = None
+myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
 
 def startFilter(stream, **kwargs):
     try:
