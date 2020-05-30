0d15af5febd2f553f6b85fc1d64645179909e909
chuliang014 <chuliang014@gmail.com>
2020-05-30 14:13:54 -0700

    update search: ranking score by relevance and time

--- src/com/lucene/search/search.java
+++ src/com/lucene/search/search.java
@@ -7,17 +7,18 @@ import java.util.Map;
 import java.util.concurrent.Executors;
 
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.core.KeywordAnalyzer;
-import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
-import org.apache.lucene.analysis.miscellaneous.PerFieldAnalyzerWrapper;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.queryparser.classic.QueryParser;
+
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.Sort;
+import org.apache.lucene.search.SortField;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
@@ -33,53 +34,17 @@ import java.net.InetSocketAddress;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 
-public class search {
+public class textSearch {
+	final static String indexDir = "index";
 
-	final static String indexDir = "Index";
-
-	public static String search(String indexDir, String q) throws Exception {
-
-		Directory dir = FSDirectory.open(new File(indexDir));
-
-		IndexReader reader = DirectoryReader.open(dir);
-
-		IndexSearcher indexSearch = new IndexSearcher(reader);
-
-		Map<String, Analyzer> analyzerPerField = new HashMap<String, Analyzer>();
-		analyzerPerField.put("text", new KeywordAnalyzer());
-		PerFieldAnalyzerWrapper aWrapper = new PerFieldAnalyzerWrapper(new WhitespaceAnalyzer(Version.LUCENE_47),
-				analyzerPerField);
-
-		QueryParser parser = new MultiFieldQueryParser(Version.LUCENE_47, new String[] { "text", "" }, aWrapper);
-		Query query = parser.parse(q);
-
-		long start = System.currentTimeMillis();
-
-		TopDocs hits = indexSearch.search(query, 50);
-
-		long end = System.currentTimeMillis();
-
-		System.out.println("Searching " + q + " totalTime is " + (end - start) + "ms" + " and the items searched are "
-				+ hits.totalHits);
-
-		// vincent array data for putting searching data as json format
-		JSONArray array = new JSONArray();
-		JSONObject data = new JSONObject();
-
-		for (ScoreDoc scoreDoc : hits.scoreDocs) {
-			Document doc = indexSearch.doc(scoreDoc.doc);
-			JSONObject item = new JSONObject();
-			// vincent putting data to item corresponding with key like title, url, body.
-			item.put("title", doc.get("header").replaceAll("\"", ""));
-			item.put("url", doc.get("url").replaceAll("\"", ""));
-			item.put("body", doc.get("body").replace("\"", "").replace("\\n", ""));
-			array.put(item);
-		}
-
-		data.put("data", array);
-		reader.close();
-
-		return data.toString();
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		HttpServerProvider provider = HttpServerProvider.provider();
+		HttpServer httpserver = provider.createHttpServer(new InetSocketAddress(2019), 200);
+		httpserver.createContext("/", new MyResponseHandler());
+		httpserver.setExecutor(Executors.newCachedThreadPool());
+		httpserver.start();
+		System.out.println("server started");
 	}
 
 	public static class MyResponseHandler implements HttpHandler {
@@ -91,12 +56,18 @@ public class search {
 
 			String requestMethod = exchange.getRequestMethod();
 
+			Headers headers = exchange.getResponseHeaders();
+			headers.set("Content-Type", "application/json; charset=utf-8");
+			headers.set("Access-Control-Allow-Origin", "*");
+			headers.set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+			headers.set("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept");
+
 			if (requestMethod.equalsIgnoreCase("GET")) {
 				System.out.println("get request");
 				Headers responseHeader = exchange.getResponseHeaders();
 				responseHeader.set("Content-Type", "text/html;charset=utf-8");
 
-				// String response = "this is server";
+//				String response = "this is server";
 
 				Map<String, String> parms = queryToMap(exchange.getRequestURI().getQuery());
 
@@ -113,12 +84,6 @@ public class search {
 
 				exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, data.getBytes("UTF-8").length);
 
-				Headers headers = exchange.getResponseHeaders();
-				headers.set("Content-Type", "application/json; charset=utf-8");
-				headers.set("Access-Control-Allow-Origin", "*");
-				headers.set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-				headers.set("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept");
-
 				OutputStream responseBody = exchange.getResponseBody();
 				OutputStreamWriter writer = new OutputStreamWriter(responseBody, "UTF-8");
 				System.out.println(data);
@@ -129,6 +94,54 @@ public class search {
 
 		}
 
+	}
+
+	private static String search(String indexDir, String q) throws Exception {
+		// TODO Auto-generated method stub
+		Directory dir = FSDirectory.open(new File(indexDir));
+		IndexReader reader = DirectoryReader.open(dir);
+
+		IndexSearcher indexSearch = new IndexSearcher(reader);
+
+		Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_47);
+
+		QueryParser parser = new MultiFieldQueryParser(Version.LUCENE_47, new String[] { "text", "title" }, analyzer);
+
+		Query query = parser.parse(q);
+
+		long start = System.currentTimeMillis();
+
+		// first sort by score of relevance
+		// second sort by datetime.
+		Sort sort = new Sort(new SortField("text", SortField.Type.SCORE),
+				new SortField("datetime", SortField.Type.STRING, true));
+		// The last two parameters:
+		// doDocScores = true means that you only score first n documents
+		// doMaxScore = true means that you need to score all of documents you search
+		TopDocs hits = indexSearch.search(query, null, 10, sort, true, false);
+
+		long end = System.currentTimeMillis();
+
+		System.out.println("Searching " + q + " ，totalTime is " + (end - start) + "ms" + " and the items searched are "
+				+ hits.totalHits);
+
+		// vincent array data for putting searching data as json format
+		JSONArray array = new JSONArray();
+		JSONObject data = new JSONObject();
+
+		for (ScoreDoc scoreDoc : hits.scoreDocs) {
+			Document doc = indexSearch.doc(scoreDoc.doc);
+			JSONObject item = new JSONObject();
+			// vincent putting data to item corresponding with key like text, title,
+			item.put("text", doc.get("text"));
+			item.put("created_at", doc.get("created_at"));
+			item.put("score", Math.round(scoreDoc.score * 10000.0) / 10000.0);
+			array.put(item);
+			System.out.println(scoreDoc.score);
+		}
+		data.put("data", array);
+		reader.close();
+		return data.toString();
 	}
 
 	public static Map<String, String> queryToMap(String query) {
@@ -142,18 +155,6 @@ public class search {
 			}
 		}
 		return result;
-	}
-
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-
-		HttpServerProvider provider = HttpServerProvider.provider();
-		HttpServer httpserver = provider.createHttpServer(new InetSocketAddress(2019), 200);
-		httpserver.createContext("/", new MyResponseHandler());
-		httpserver.setExecutor(Executors.newCachedThreadPool());
-		httpserver.start();
-		System.out.println("server started");
-
 	}
 
 }
